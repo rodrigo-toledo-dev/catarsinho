8ef0130a83e1cd290c3aae8272a4d3fc32138fd4
Rodrigo Toledo <rodrigo.toledo.dev@gmail.com>
2019-06-01 17:46:47 -0300

    details of project and contribution for him

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.friendly.find(params[:id])
+    @project_item = @project.project_items.build
     respond_to do |format|
       format.js
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'visitor_actions/sign_in'
   get 'visitor_actions/sign_out'
   root 'home#index'
-  resources :projects
+  resources :projects do
+    resources :project_items
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
