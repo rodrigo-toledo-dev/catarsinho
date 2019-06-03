1310da26f8b41c26bba08d03e77b32487b23ee70
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-06-02 22:56:19 -0300

    new actios to save the project and details of it validatitors to project and project item

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,29 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def new
+    @project = Project.new
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      logger.info "================= salvou"
+      logger.info @project.inspect
+      byebug
+    else
+      logger.info "================= nao foi"
+      logger.info @project.errors.inspect
+      byebug
+    end
+    redirect_to root_path
+  end
+
+  protected
+    def project_params
+      params.require(:project).permit(:title, :description, :image, :value_to_get)
+    end
 end
