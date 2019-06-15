4c390ee80b8292bb010b2c625073cce0f6f9cc26
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-06-15 00:23:56 -0300

    fix the project real time funcionality for all browsers effects to show for the user what its happinning

--- app/jobs/project_broadcast_job.rb
+++ app/jobs/project_broadcast_job.rb
@@ -1,8 +1,6 @@
 class ProjectBroadcastJob < ApplicationJob
   queue_as :default
   def perform(project)
-    logger.info "-----------------------------------"
-    logger.info project.inspect
     ActionCable.server.broadcast 'project_channel', content: render_project(project), slug: project.slug
   end
 
