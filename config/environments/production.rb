f6f96bc1ace3e488ffa604b484d2b42d5ea8e393
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-06-15 00:35:03 -0300

    first steps to heroku with real time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.web_socket_server_url = "wss://catarsinho.herokuapp.com/cable"
+  config.action_cable.url = 'wss://catarsinho.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://catarsinho.herokuapp.com', 'http://catarsinho.herokuapp.com']
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
